e506b6e0fbd65d1910057161562cfe23f3cf0aa4
Reinaldo44 <reinaldoap82@hotmail.com>
2022-03-12 14:53:23 -0300

    Alterações, Create Construtor

--- Exercicio_Visibilidade/Notas.java
+++ Exercicio_Visibilidade/Notas.java
@@ -1,11 +1,34 @@
 /**
- * Notas
+ * Classe Notas, responsável por criar as notas de alunos, com 3 atributos private, um construtor que requerer 3 parâmetros para criar um objeto Notas, e possui os metodos set e get para ascessar os atributos, e o método para calcular a média.
  */
 public class Notas {
 
+   //Propriedades privada 
    private double Nota1;
    private double Nota2;
+   private int Faltas;
 
+   //Construtor de Notas
+   public Notas(double nota1, double nota2, int faltas){
+        setNota1(nota1);
+        setNota2(nota2);
+        setFaltas(faltas);   
+    }
+   
+   //Método set para fazer validação e ter acesso ao atributo
+   public void setFaltas(int faltas) {
+       if(faltas > 40 || faltas < 0){
+        Faltas = faltas;
+       }
+       
+   }
+
+   //método get para acessar as propriedades
+   public int getFaltas() {
+       return Faltas;
+   }
+
+   //Método set para fazer validação e ter acesso ao atributo
    public void setNota1(double nota1) {
 
        if(nota1 < 0 || nota1 > 10){
@@ -14,6 +37,7 @@ public class Notas {
        Nota1 = nota1;
      }
 
+   //Método set para fazer validação e ter acesso ao atributo
    public void setNota2(double nota2) {
     if(nota2 < 0 || nota2 > 10){
         System.out.println("Nota inválida");
@@ -21,22 +45,28 @@ public class Notas {
        Nota2 = nota2;
      }
 
+   //método get para acessar as propriedades
    public double getNota1() {
 
        return Nota1;
     
     }
      
- 
+   //método get para acessar as propriedades
    public double getNota2() {
        return Nota2;
      }
 
+    //método que calcula a média de notas
     void media(){
 
         double media = Nota1 + Nota2 / 2;
+
+        if(Faltas < 10){
+            System.out.println("Aluno reprovado por faltas");
+        }
        
-        if(media < 4){
+        else if(media < 4){
            System.out.println("Aluno reprovado");
         }
         else if(media < 7){
@@ -46,8 +76,13 @@ public class Notas {
             System.out.println("Aluno aprovado");
         }
 
+        System.out.println("A sua média foi: " + media);
+
+
     }
 
 
-  
 }
+
+
+  
--- Exercicio_Visibilidade/Principal.java
+++ Exercicio_Visibilidade/Principal.java
@@ -1,17 +1,17 @@
 public class Principal {
     public static void main(String[] args) {
+        //Cria um novo objeto mario com tres parametros
+        Notas mario = new Notas(5, 6, 11);
+
+       // mario.setNota1(5);
+       // mario.setNota2(8);
+       // mario.setFaltas(11);
+
         
-        Notas mario = new Notas();
-
-        mario.setNota1(5);
-        mario.setNota2(8);
-
         System.out.println("A nota 1 foi: " + mario.getNota1());
         System.out.println("A nota 2 foi: " + mario.getNota2());
    
-        mario.media();
-
-        
+        mario.media();        
 
     }
 }
